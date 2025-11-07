6dbc9f0db84bc97041820fd97b314b02840599a1
Ryan Lokugamage <rlokugamage@fanthreesixty.com>
2025-11-07 08:31:37 -0600

    FAN360-8496 Added integrations.tm_full_pull_history table

---
+++ src/main/resources/db/changelogs/008-create-tm-full-pull-history.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+
+--changeset rlokugamage:008-create-tm-full-pull-history
+--comment: create table tm_full_pull_history
+CREATE TABLE integrations.tm_full_pull_history (
+    id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    file_path TEXT NOT NULL,
+    s3_location TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    metadata JSONB
+)
