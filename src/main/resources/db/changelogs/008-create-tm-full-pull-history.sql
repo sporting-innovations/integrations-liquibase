87aee648b212e665d12a1019176806b61ab9af8d
Ryan Lokugamage <rlokugamage@fanthreesixty.com>
2025-11-07 16:01:21 -0600

    applying UNIQUE constraint on file_path column

--- src/main/resources/db/changelogs/008-create-tm-full-pull-history.sql
+++ src/main/resources/db/changelogs/008-create-tm-full-pull-history.sql
@@ -4,7 +4,7 @@
 --comment: create table tm_full_pull_history
 CREATE TABLE integrations.tm_full_pull_history (
     id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
-    file_path TEXT NOT NULL,
+    file_path TEXT NOT NULL UNIQUE,
     s3_location TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
